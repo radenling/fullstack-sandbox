516f37366049428c22ace63852f04a2f41d92da1
Richard Adenling <dreeze@gmail.com>
2022-10-21 23:30:08 +0200

    Load static lists from backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,4 +9,19 @@ const PORT = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/lists', (req, res) => {
+  res.json({
+    '0000000001': {
+      id: '0000000001',
+      title: 'First List',
+      todos: ['First todo of first list!'],
+    },
+    '0000000002': {
+      id: '0000000002',
+      title: 'Second List',
+      todos: ['First todo of second list!'],
+    },
+  })
+})
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -15,20 +15,17 @@ import { TodoListForm } from './TodoListForm'
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const fetchTodoLists = () => {
-  return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: ['First todo of first list!'],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: ['First todo of second list!'],
-      },
+  return fetch('http://localhost:3001/lists')
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+
+      return response.json()
     })
-  )
+    .catch((error) => {
+      console.error(error)
+    })
 }
 
 export const TodoLists = ({ style }) => {
